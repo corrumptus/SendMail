325095385ded7fc9ec4dd2ecb1ce48de212f8648
corrumptus <lazarinilucas42@gmail.com>
2024-05-13 17:12:00 -0300

    feat: adding clear button click event handler

--- app/src/main/java/lazarini/lucas/sendmail/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/sendmail/MainActivity.kt
@@ -1,5 +1,15 @@
 package lazarini.lucas.sendmail
 
+import android.content.Intent
+import android.content.Intent.ACTION_CHOOSER
+import android.content.Intent.ACTION_SENDTO
+import android.content.Intent.EXTRA_BCC
+import android.content.Intent.EXTRA_CC
+import android.content.Intent.EXTRA_EMAIL
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.EXTRA_SUBJECT
+import android.content.Intent.EXTRA_TEXT
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.sendmail.databinding.ActivityMainBinding
@@ -12,5 +22,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        activityMainBinding.clearBt.setOnClickListener {
+            activityMainBinding.apply {
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                messageEt.setText("")
+            }
+        }
     }
 }
